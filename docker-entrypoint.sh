3338309ec49952f002a041e367439df7a8044f39
Fabio Tranchitella <fabio@tranchitella.eu>
2019-11-04 23:00:59 +0100

    Enh: DMQ and dialog sharing between SBC instances

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 date
 
+IP_ADDRESS=$(hostname -i)
+
 mkdir -p /etc/kamailio/
 echo '#!define LISTEN '$LISTEN > /etc/kamailio/kamailio-local.cfg
+if ! [ -z "$WITH_DMQ" ]; then
+    echo '#!define WITH_DMQ 1' >> /etc/kamailio/kamailio-local.cfg
+    echo '#!define DMQ_PORT "'$DMQ_PORT'"' >> /etc/kamailio/kamailio-local.cfg
+    echo '#!define DMQ_LISTEN '$DMQ_LISTEN >> /etc/kamailio/kamailio-local.cfg
+    echo '#!define DMQ_SERVER_ADDRESS "sip:'$IP_ADDRESS':'$DMQ_PORT'"' >> /etc/kamailio/kamailio-local.cfg
+    echo '#!define DMQ_NOTIFICATION_ADDRESS "'$DMQ_NOTIFICATION_ADDRESS'"' >> /etc/kamailio/kamailio-local.cfg
+fi
 
 #--- KAMAILIO ---#
 export PATH_KAMAILIO_CFG=/etc/kamailio/kamailio.cfg
