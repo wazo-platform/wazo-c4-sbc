d0e42954b1b17089d5b5f1c4938d8f9ac48bef97
Aleksandar Sosic <alex.sosic@gmail.com>
2019-12-07 21:43:10 +0100

    Enh: parametrize interfaces

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,18 +5,30 @@ if ! [ -z "$CONSUL_URI" ]; then
     sleep 2
 fi
 
-if [ -z "$INTERFACE" ]; then
+if [ -z "$INTERFACE_SIP" ]; then
     INTERFACE="eth0"
 fi
-PUBLIC_IP=$(ip -o -4 a | awk '$2 == "'$INTERFACE'" { gsub(/\/.*/, "", $4); print $4 }')
+if [ -z "$INTERFACE_DMQ" ]; then
+    INTERFACE_DMQ="$INTERFACE"
+fi
+if [ -z "$INTERFACE_XHTTP" ]; then
+    INTERFACE_XHTTP="$INTERFACE"
+fi
+SIP_IP=$(ip -o -4 a | awk '$2 == "'$INTERFACE'" { gsub(/\/.*/, "", $4); print $4 }')
+DMQ_IP=$(ip -o -4 a | awk '$2 == "'$INTERFACE_DMQ'" { gsub(/\/.*/, "", $4); print $4 }')
+XHTTP_IP=$(ip -o -4 a | awk '$2 == "'$INTERFACE_XHTTP'" { gsub(/\/.*/, "", $4); print $4 }')
+
+if [ -z "$XHTTP_PORT" ]; then
+    XHTTP_PORT="8000"
+fi
 
 HOSTNAME=$(hostname)
-IP_ADDRESS=$(hostname -i)
 export KAMAILIO=$(which kamailio)
 
 mkdir -p /etc/kamailio/
 
 echo '#!define LISTEN '$LISTEN > /etc/kamailio/kamailio-local.cfg
+echo '#!define LISTEN_XHTTP tcp:'$INTERFACE_XHTTP':'$XHTTP_PORT >> /etc/kamailio/kamailio-local.cfg
 if ! [ -z "$TESTING" ]; then
     echo '#!define TESTING 1' >> /etc/kamailio/kamailio-local.cfg
 fi
@@ -27,7 +39,7 @@ if ! [ -z "$WITH_DMQ" ]; then
     echo '#!define WITH_DMQ 1' >> /etc/kamailio/kamailio-local.cfg
     echo '#!define DMQ_PORT "'$DMQ_PORT'"' >> /etc/kamailio/kamailio-local.cfg
     echo '#!define DMQ_LISTEN '$DMQ_LISTEN >> /etc/kamailio/kamailio-local.cfg
-    echo '#!define DMQ_SERVER_ADDRESS "sip:'$PUBLIC_IP':'$DMQ_PORT'"' >> /etc/kamailio/kamailio-local.cfg
+    echo '#!define DMQ_SERVER_ADDRESS "sip:'$DMQ_IP':'$DMQ_PORT'"' >> /etc/kamailio/kamailio-local.cfg
     echo '#!define DMQ_NOTIFICATION_ADDRESS "'$DMQ_NOTIFICATION_ADDRESS'"' >> /etc/kamailio/kamailio-local.cfg
 fi
 
@@ -46,14 +58,14 @@ curl -i -X PUT http://${CONSUL_URI}/v1/agent/service/register -d '{
     "ID": "'$HOSTNAME'",
     "Name": "sbc",
     "Tags": ["sbc", "kamailio"],
-    "Address": "'$PUBLIC_IP'",
+    "Address": "'$SIP_IP'",
     "Port": '$SIP_PORT',
     "Check": {
         "ID": "XHTTP",
         "Name": "XHTTP API on port 8000",
         "DeregisterCriticalServiceAfter": "10m",
         "Method": "GET",
-        "HTTP": "http://'$PUBLIC_IP':8000/status",
+        "HTTP": "http://'$XHTTP_IP':'$XHTTP_PORT'/status",
         "Timeout": "1s",
         "Interval": "10s"
     }
